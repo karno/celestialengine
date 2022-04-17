3948277410b101a92e04d5b2f162e4eeb31b662c
Karno <karnoroid@gmail.com>
2022-04-17 16:21:46 +0900

    fix of external reference for threejs examples.

---
+++ src/renderers/index.ts
@@ -0,0 +1,6 @@
+export * from "./Effector";
+export * from "./Navigator";
+export * from "./primitives/Constellations";
+export * from "./primitives/GridSpheres";
+export * from "./primitives/Labels";
+export * from "./StarRenderer";
